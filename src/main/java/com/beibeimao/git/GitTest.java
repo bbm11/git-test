faad8e706cf39ec7519f59842a356ea6ef3bb8b4
Verlit <872247283@qq.com>
2022-04-27 15:22:30 +0800

    first commit

---
+++ src/main/java/com/beibeimao/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.beibeimao.git;
+
+/**
+ * @author: beibeimao
+ * @date: 2022/4/27 15:17
+ * @description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
